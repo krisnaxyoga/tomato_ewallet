6b82bd391890a2778f211c406803336b6507115e
krisnaxyoga <yogakris.yk@gmail.com>
2024-06-20 21:59:56 +0700

    update onboarding

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -2,8 +2,27 @@ import 'package:bank_sha/shared/theme.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentIndex = 0;
+  CarouselController carouselController = CarouselController();
+  List<String> titles = [
+    'Grow Your \n Financial Today',
+    'Build From\n Zero to Freedom',
+    'Start Together'
+  ];
+
+  List<String> subtitles = [
+    'Our system is helping you to \n achieve a better goal',
+    'We provide tips for you so that\nyou can adapt easier',
+    'We will guide you to where\nyou wanted it too'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,24 +37,30 @@ class OnboardingPage extends StatelessWidget {
                 items: [
                   Image.asset(
                     'assets/img_onboarding1.png',
-                    height: 331,
+                    height: 250,
                   ),
                   Image.asset(
                     'assets/img_onboarding2.png',
-                    height: 331,
+                    height: 250,
                   ),
                   Image.asset(
                     'assets/img_onboarding3.png',
-                    height: 331,
+                    height: 250,
                   ),
                 ],
                 options: CarouselOptions(
-                    height: 331,
+                    onPageChanged: (index, reason) => {
+                          setState(() {
+                            currentIndex = index;
+                          })
+                        },
+                    height: 250,
                     viewportFraction: 1,
                     enableInfiniteScroll: false),
+                carouselController: carouselController,
               ),
               const SizedBox(
-                height: 80,
+                height: 60,
               ),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 24),
@@ -48,10 +73,10 @@ class OnboardingPage extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      'Grow Your \n Financial Today',
+                      titles[currentIndex],
                       style: blackTextStyle.copyWith(
-                        fontSize: 20,
-                        fontWeight: medium,
+                        fontSize: 15,
+                        fontWeight: bold,
                       ),
                       textAlign: TextAlign.center,
                     ),
@@ -59,57 +84,107 @@ class OnboardingPage extends StatelessWidget {
                       height: 26,
                     ),
                     Text(
-                      'Our system is helping you \n to achieve a better goal',
-                      style: greyTextStyle.copyWith(fontSize: 16),
+                      subtitles[currentIndex],
+                      style: greyTextStyle.copyWith(fontSize: 14),
                       textAlign: TextAlign.center,
                     ),
-                    const SizedBox(
-                      height: 50,
+                    SizedBox(
+                      height: currentIndex == 2 ? 30 : 40,
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 12,
-                          height: 12,
-                          margin: const EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle, color: blueColor),
-                        ),
-                        Container(
-                          width: 12,
-                          height: 12,
-                          margin: const EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: lightBackgoundColor),
-                        ),
-                        Container(
-                          width: 12,
-                          height: 12,
-                          margin: const EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: lightBackgoundColor),
-                        ),
-                        const Spacer(),
-                        Container(
-                          width: 150,
-                          height: 50,
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                                backgroundColor: purpleColor,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(56))),
-                            onPressed: () {},
-                            child: Text(
-                              'continue',
-                              style: whiteTextStyle.copyWith(
-                                  fontSize: 16, fontWeight: medium),
-                            ),
-                          ),
-                        )
-                      ],
-                    )
+                    currentIndex == 2
+                        ? Column(
+                            children: [
+                              SizedBox(
+                                width: double.infinity,
+                                height: 45,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: purpleColor,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(56))),
+                                  onPressed: () {
+                                    carouselController.nextPage();
+                                  },
+                                  child: Text(
+                                    'Get Started',
+                                    style: whiteTextStyle.copyWith(
+                                        fontSize: 15, fontWeight: medium),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 24,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      padding: EdgeInsets.zero),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Sign in',
+                                    style: greyTextStyle.copyWith(fontSize: 14),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        : Row(
+                            children: [
+                              Container(
+                                width: 10,
+                                height: 10,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: currentIndex == 0
+                                        ? blueColor
+                                        : lightBackgoundColor),
+                              ),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: currentIndex == 1
+                                        ? blueColor
+                                        : lightBackgoundColor),
+                              ),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: currentIndex == 2
+                                        ? blueColor
+                                        : lightBackgoundColor),
+                              ),
+                              const Spacer(),
+                              SizedBox(
+                                width: 125,
+                                height: 45,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: purpleColor,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(56))),
+                                  onPressed: () {
+                                    carouselController.nextPage();
+                                  },
+                                  child: Text(
+                                    'continue',
+                                    style: whiteTextStyle.copyWith(
+                                        fontSize: 15, fontWeight: medium),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
                   ],
                 ),
               )
