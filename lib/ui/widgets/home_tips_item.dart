db6cbcb64288e4e1fbab85c0472e2fb65a205352
muliadev3 <softhouse3@themulia.com>
2024-06-21 17:06:47 +0800

    update tips page

---
+++ lib/ui/widgets/home_tips_item.dart
@@ -0,0 +1,58 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomeTipsItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String url;
+
+  const HomeTipsItem({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.url,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 127,
+      height: 176,
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+            child: Image.asset(
+              imageUrl,
+              width: 155,
+              height: 110,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Text(
+              title,
+              style: blackTextStyle.copyWith(
+                fontWeight: medium,
+                overflow: TextOverflow.ellipsis,
+              ),
+              maxLines: 2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
